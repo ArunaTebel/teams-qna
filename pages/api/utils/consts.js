9b9bcb88c6450d6dc3025193d6ff4b5319f808df
ArunaTebel <arunatebel@hotmail.com>
2020-04-10 02:45:08 +0530

    adding prod API server details to const.js

--- pages/api/utils/consts.js
+++ pages/api/utils/consts.js
@@ -1,13 +1,13 @@
 export default {
-    "API_BASE": 'http://127.0.0.1:8000',
+    "API_BASE": 'https://arch-teams-qna.herokuapp.com',
     "API_BASE_API_PATH": 'api',
-    "API_PATH": 'http://127.0.0.1:8000/api',
+    "API_PATH": 'https://arch-teams-qna.herokuapp.com/api',
     "API_BASE_AUTH_PATH": 'auth',
     "OAUTH": {
         "grant_type": "authorization_code",
-        "client_id": "a0kXnnHLo9t2Wj4hhDujdLjoVoh92NsUov1rwLTj",
-        "client_secret": "w1h1VhkRAdBKckHddlGqKEELZpET9BD8sVRFTWkfp6vh2FPMgYxGLqVmCw62mYTZnOPc2Mr7yyAl0gTfyxxdDNNYKVB9CKBwQkJiXXgXWnG3SL1L395Y5pgsYhFL0MQY",
-        "redirect_uri": "http://localhost:3000/auth/login-callback",
+        "client_id": "WtDoiOO2jbY0fnfbgykxHQgCQZU18Lql4Vwxae2g",
+        "client_secret": "fKaYqYqhxluE0uBq20bLeWJorVguVHE3z5Jn0orgy1tLzIEIWoH3FayOhM5YgpYx3IdjUZkDcaXF0qJw0ZwdTeurch9YjNvG0nuieBLCy2CzJWMwaY1j3t3zGggjYVTi",
+        "redirect_uri": "https://teams-qna.now.sh/auth/login-callback",
         "access_token_cookie_name": "access-token"
     }
 }
