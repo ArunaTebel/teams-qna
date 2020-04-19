bf45632e84ee4f0816889ca61ca0f86ce61cc203
ArunaTebel <arunatebel@hotmail.com>
2020-04-19 18:19:08 +0530

    removing unused import

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import 'semantic-ui-css/semantic.min.css'
 import 'react-toastify/dist/ReactToastify.css'
-import _ from 'lodash'
 import {toast} from 'react-toastify';
 
 export default function App({Component, pageProps}) {
