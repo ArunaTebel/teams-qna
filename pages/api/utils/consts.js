5c7acb38758e77b38c6cd4cb84245323ee88a991
ArunaTebel <arunatebel@hotmail.com>
2020-04-22 22:13:57 +0530

    adding production credentials to the constants file

--- pages/api/utils/consts.js
+++ pages/api/utils/consts.js
@@ -1,13 +1,13 @@
 export default {
-    "API_BASE": 'http://127.0.0.1:8000',
+    "API_BASE": 'https://arch-teams-qna.herokuapp.com',
     "API_BASE_API_PATH": 'api',
-    "API_PATH": 'http://127.0.0.1:8000/api',
+    "API_PATH": 'https://arch-teams-qna.herokuapp.com/api',
     "API_BASE_AUTH_PATH": 'auth',
     "OAUTH": {
         "grant_type": "implicit",
         "response_type": "token",
         "client_id": "UJ90Zr4AQlRbW5d7DZMWrx2LDqAOzrlVPsz8Rx2N",
-        "redirect_uri": "http://localhost:3000/auth/login-callback",
+        "redirect_uri": "https://teams-qna.now.sh/auth/login-callback",
         "access_token_cookie_name": "access-token"
     }
 }
