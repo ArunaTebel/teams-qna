0e2b128fdc06de423f35b8b106905f199f08e26c
ArunaTebel <arunatebel@hotmail.com>
2023-05-23 19:32:04 +0530

    Change client id and API urls

--- pages/api/utils/consts.js
+++ pages/api/utils/consts.js
@@ -1,13 +1,13 @@
 export default {
-    "API_BASE": 'https://arch-teams-qna.herokuapp.com',
+    "API_BASE": 'https://teams-qna-api-prod.vercel.app',
     "API_BASE_API_PATH": 'api',
-    "API_PATH": 'https://arch-teams-qna.herokuapp.com/api',
+    "API_PATH": 'https://teams-qna-api-prod.vercel.app/api',
     "API_BASE_AUTH_PATH": 'auth',
     "OAUTH": {
         "grant_type": "implicit",
         "response_type": "token",
-        "client_id": "UJ90Zr4AQlRbW5d7DZMWrx2LDqAOzrlVPsz8Rx2N",
-        "redirect_uri": "https://teams-qna.now.sh/auth/login-callback",
+        "client_id": "yN1ivJ0y74KM4xJvoM5iuholW6SO15HA4IShlIkb",
+        "redirect_uri": "https://teams-archeun.vercel.app//auth/login-callback",
         "access_token_cookie_name": "access-token"
     }
 }
